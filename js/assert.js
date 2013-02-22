08382edd27cc7a66b3713e915eda3a1dcfe4a8f9
Jonathan Olson <olsonsjc@gmail.com>
2013-02-21 22:21:11 -0800

    Adding assert.js (initial version)

---
+++ js/assert.js
@@ -0,0 +1,39 @@
+// Copyright 2002-2012, University of Colorado
+
+/*
+ * Usage:
+ * var assert = require( '<assert>' )( 'flagName' );
+ *
+ * assert && assert( <simple value or big computation>, "<message here>" );
+ *
+ * TODO: decide on usages and viability, and if so document further
+ *
+ * @author Jonathan Olson <olsonsjc@gmail.com>
+ */
+
+define( function( require ) {
+  // this variable should be replaced by our minification engine (Uglify / Closure) for production builds
+  var phetDebugFlagBuild = 'development';
+  
+  // pull the flags for our specific build type, or default to no flags if it is not specified
+  var flags = window.phetDebugFlags ? window.phetDebugFlags[ phetDebugFlagBuild ] : {};
+  
+  var assert = function( name, excludeByDefault ) {
+    var flagDefined = flags && flags[name] !== undefined;
+    
+    if ( flagDefined ? !flags[name] : excludeByDefault ) {
+      return null;
+    } else {
+      return function( predicate, message ) {
+        var result = typeof predicate === 'function' ? predicate() : predicate;
+        
+        if ( !result ) {
+          // TODO: custom error?
+          throw new Error( 'Assertion failed: ' + message );
+        }
+      };
+    }
+  };
+  
+  return assert;
+} );
