321768d4e876c3bd42b712066affa53f992cc461
Jonathan Olson <olsonsjc@gmail.com>
2014-11-12 00:01:47 -0700

    Changing assertions to the preloaded assert/js/assert.js (which should be included in any HTML file that will include JS running our assertions). see https://github.com/phetsims/scenery/issues/300

--- js/assert.js
+++ js/assert.js
@@ -1,48 +1,46 @@
 // Copyright 2002-2014, University of Colorado Boulder
 
 /*
- * Usage:
- * var assert = require( '<assert>' )( 'flagName' );
- *
- * assert && assert( <simple value or big computation>, "<message here>" );
- *
- * TODO: decide on usages and viability, and if so document further
- *
- * NOTE: for changing build, add has.js tests for 'assert.' + flagName
- *
  * @author Jonathan Olson <jonathan.olson@colorado.edu>
  */
 
-define( function() {
+(function(){
   'use strict';
 
-  // CAUTION: if using the AST modifier for assertions, do not separate this out into its own 'assert' variable
-  return function assert( name, excludeByDefault ) {
-    var hasName = 'assert.' + name;
+  window.assertions = window.assertions || {};
+  window.assertions.assertFunction = window.assertions.assertFunction || function( predicate, message ) {
+    var result = typeof predicate === 'function' ? predicate() : predicate;
 
-    var flagDefined = window.has && window.has( hasName ) !== undefined;
-    var skipAssert = flagDefined ? !window.has( hasName ) : excludeByDefault;
+    if ( !result ) {
 
-    if ( skipAssert ) {
-      return null;
-    }
-    else {
-      return function( predicate, message ) {
-        var result = typeof predicate === 'function' ? predicate() : predicate;
+      //Log the stack trace to IE.  Just creating an Error is not enough, it has to be caught to get a stack.
+      if ( window.navigator && window.navigator.appName === 'Microsoft Internet Explorer' ) {
+        try { throw new Error(); }
+        catch( e ) { message = message + ', stack=\n' + e.stack; }
+      }
 
-        if ( !result ) {
-
-          //Log the stack trace to IE.  Just creating an Error is not enough, it has to be caught to get a stack.
-          //TODO: What will this do for IE9?  Probably just print stack = undefined.
-          if ( window.navigator && window.navigator.appName === 'Microsoft Internet Explorer' ) {
-            try { throw new Error(); }
-            catch( e ) { message = message + ", stack=\n" + e.stack; }
-          }
-
-          // TODO: custom error?
-          throw new Error( 'Assertion failed: ' + message );
-        }
-      };
+      throw new Error( 'Assertion failed: ' + message );
     }
   };
-} );
+
+  window.assert = window.assert || null;
+  window.assertSlow = window.assertSlow || null;
+
+  window.assertions.enableAssert = function() {
+    window.assert = window.assertions.assertFunction;
+    window.console && window.console.log && window.console.log( 'enabling assert' );
+  };
+  window.assertions.disableAssert = function() {
+    window.assert = null;
+    window.console && window.console.log && window.console.log( 'disabling assert' );
+  };
+
+  window.assertions.enableAssertSlow = function() {
+    window.assertSlow = window.assertions.assertFunction;
+    window.console && window.console.log && window.console.log( 'enabling assertSlow' );
+  };
+  window.assertions.disableAssertSlow = function() {
+    window.assertSlow = null;
+    window.console && window.console.log && window.console.log( 'disabling assertSlow' );
+  };
+})();
